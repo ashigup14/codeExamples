c03b44da5a2d30810a9a55fdc949ca6e55832d0a
z002y1ra <z002y1ra@md1zttmc.ad001.siemens.net>
2020-02-06 21:59:41 +0530

    IMP: Added a scenario for orElse and orElseGet

--- src/com/test/AlgoTest.java
+++ src/com/test/AlgoTest.java
@@ -1,10 +1,25 @@
 package com.test;
 
+import java.util.Optional;
+
 public class AlgoTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		AlgoTest altest = new AlgoTest();
+		System.out.println(altest.findUserStatus(10l));
 
+	}
+	
+	// AVOID
+	public String computeStatus() {
+	    System.out.println("in computeStatus");// some code used to compute status
+	    return "computeStatus result";
+	}
+	public String findUserStatus(long id) {
+	    Optional<String> status = Optional.of("Result"); // prone to return an empty Optional
+	    // computeStatus() is called even if "status" is not empty
+	    // return status.orElse(computeStatus()); 
+	    return status.orElseGet(this::computeStatus); 
 	}
 
 }

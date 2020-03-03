e3659dd6d83240531490fedc3f598818032333ea
ashigup14 <ashigup14@gmail.com>
2020-03-03 23:11:39 +0530

    IMP: Added new example

--- src/com/test/lambda/MapTest.java
+++ src/com/test/lambda/MapTest.java
@@ -2,6 +2,8 @@ package com.test.lambda;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MapTest {
@@ -9,6 +11,8 @@ public class MapTest {
 	public static void main(String[] args) {
 		
 		map_times_10();
+		
+		map_times_10_refactor();
 	}
 	
 	public static void map_times_10() {
@@ -16,6 +20,17 @@ public class MapTest {
 	    List<Integer> myList = Arrays.asList(1, 2, 3, 4, 5);
 
 	    Stream<Integer> multipleOf10 = myList.stream().map((x) -> x * 10);
+
+	    multipleOf10.forEach(System.out::println);
+	}
+	
+	public static void map_times_10_refactor() {
+
+	    List<Integer> myList = Arrays.asList(1, 2, 3, 4, 5);
+	    Function<Integer, Integer> times10 = (x) -> x * 10;
+
+	    List<Integer> multipleOf10 = myList.stream().map(times10)
+	            .collect(Collectors.toList());
 
 	    multipleOf10.forEach(System.out::println);
 	}

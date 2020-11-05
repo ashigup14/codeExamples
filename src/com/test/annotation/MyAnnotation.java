9bc152a45d3192ef851606bc3815dae5d020aac4
ashigup14 <ashigup14@gmail.com>
2020-11-05 12:10:37 +0530

    IMP:Update

--- src/com/test/annotation/MyAnnotation.java
+++ src/com/test/annotation/MyAnnotation.java
@@ -3,10 +3,18 @@
  */
 package com.test.annotation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
 /**
  * @author Ashish Gupta
  *
  */
+
+//restricting annotations 
+@Target(ElementType.TYPE)
+@Documented
 public @interface MyAnnotation {
 
 	String name() default "rahul";

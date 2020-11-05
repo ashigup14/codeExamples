f487e21da1e180b447da06a81a636d9acb6595a8
ashigup14 <ashigup14@gmail.com>
2020-11-05 11:45:49 +0530

    IMP: Added example for annotation

---
+++ src/com/test/annotation/AnnotationExample.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.test.annotation;
+
+/**
+ * @author ashish
+ *
+ */
+@MyAnnotation(name = "Ashish", count = 10)
+public class AnnotationExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/com/test/annotation/MyAnnotation.java
+++ src/com/test/annotation/MyAnnotation.java
@@ -7,6 +7,8 @@ package com.test.annotation;
  * @author Ashish Gupta
  *
  */
-public interface MyAnnotation {
+public @interface MyAnnotation {
 
+	String name() default "rahul";
+	int count() default 100;
 }

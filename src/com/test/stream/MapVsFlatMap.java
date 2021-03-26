5d6b2813239dab3f74ce14c807527737645ba9fa
ashigup14 <ashigup14@gmail.com>
2021-03-27 02:34:45 +0530

    IMP: Streams examples

--- src/com/test/stream/MapVsFlatMap.java
+++ src/com/test/stream/MapVsFlatMap.java
@@ -3,7 +3,9 @@
  */
 package com.test.stream;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -33,6 +35,15 @@ public class MapVsFlatMap {
 											 .flatMap(cus -> cus.getPhoneNumbers().stream())
 											 .collect(Collectors.toList());
 		System.out.println(phoneNumbers);
+		
+		List<Integer> numbers =  Arrays.asList(1,2,3,4,5,6,7);
+		Optional<Integer> findingMax = numbers.stream().reduce(Integer::max);
+		Integer maxNumber = findingMax.isPresent()?findingMax.get():0;
+		System.out.println(maxNumber);
+		
+		List<String> chars =  Arrays.asList("a","s","h","i","j");
+		String stringsAdded = chars.stream().reduce("", (a,b) -> a+b);
+		System.out.println(stringsAdded);
 	}
 
 }

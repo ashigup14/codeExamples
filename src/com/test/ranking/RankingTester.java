7eb2843ea684574e86350b024546ff54dc4c2d36
ashigup14 <ashigup14@gmail.com>
2021-03-31 15:10:10 +0530

    iMP: Ranking test update

--- src/com/test/ranking/RankingTester.java
+++ src/com/test/ranking/RankingTester.java
@@ -5,9 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class RankingTester {
 
